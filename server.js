c1fa818e873868299911f9060d64bad909ad7a70
arnabtechie <arnabb.bannerjee@gmail.com>
2023-04-08 12:57:41 +0530

    cluster removed

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
 const app = require('./app');
 const config = require('./config');
-const cluster = require('cluster');
 require('./db');
 
-const numCPUs = require('os').cpus().length;
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err);
+    process.exit(1);
+});
 
-if (cluster.isMaster) {
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork();
-    }
-} else {
-    process.on('uncaughtException', err => {
-        console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-        console.log(err);
-        process.exit(1);
+const server = app.listen(config.PORT, () => {
+    console.log(`server running on port ${config.PORT}...`);
+});
+
+process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+        console.log('💥 Process terminated!');
     });
-
-    const server = app.listen(config.PORT, () => {
-        console.log(`server running on port ${config.PORT}...`);
-    });
-
-    process.on('SIGTERM', () => {
-        console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-        server.close(() => {
-            console.log('💥 Process terminated!');
-        });
-    });
-
-    console.log(`Worker ${process.pid} started`);
-}
+});
